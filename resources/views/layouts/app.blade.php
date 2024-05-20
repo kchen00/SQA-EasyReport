005b9c7cd21cb9bbe7edae489c48dd33cfa5d73f
kalvin chen <kalvin412@gmail.com>
2024-05-20 16:47:08 +0800

    removed unused files, added new logo and icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="apple-touch-icon" sizes="76x76" href="/img/apple-icon.png">
-    <link rel="icon" type="image/png" href="/img/favicon.png">
+    <link rel="icon" type="image/png" href="{{ asset("./img/icon.png") }}">
     <title>
         Easy Report
     </title>
